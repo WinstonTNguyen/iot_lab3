b3e84b4253be580c80725dc7ee38c9f960dfdc24
WinstonTNguyen <52378568+WinstonTNguyen@users.noreply.github.com>
2022-04-29 17:20:08 +0700

    Final version

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function sendTEMP () {
+    countTEMP = sec
+    serial.writeString("!1:TEMP:" + input.temperature() + "#")
+}
 input.onButtonPressed(Button.A, function () {
     serial.writeString("I'm alive!")
 })
@@ -7,30 +11,92 @@ serial.onDataReceived(serial.delimiters(Delimiters.Hash), function () {
     if (cmd == "0") {
         basic.clearScreen()
     } else if (cmd == "1") {
-        basic.showIcon(IconNames.No)
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
     } else if (cmd == "2") {
-        basic.showIcon(IconNames.Yes)
+        basic.showLeds(`
+            # # # # #
+            . # . # .
+            . # . # .
+            . # . # .
+            # # # # #
+            `)
     } else if (cmd == "3") {
-        basic.showIcon(IconNames.Silly)
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            `)
     } else if (cmd == "4") {
-        basic.showIcon(IconNames.StickFigure)
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
+            # . . # .
+            `)
     } else if (cmd == "LED_true") {
-    	
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # . # #
+            . # # # .
+            # . # . #
+            `)
     } else if (cmd == "LED_false") {
-    	
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
     } else if (cmd == "FAN_true") {
-    	
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
     } else if (cmd == "FAN_false") {
-    	
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
     } else {
         basic.showString("xx")
     }
 })
-let cmd = ""
-basic.showIcon(IconNames.Heart)
-basic.forever(function () {
-    serial.writeString("!1:TEMP:" + input.temperature() + "#")
-    basic.pause(5000)
+function sendLIGHT () {
+    countLIGHT = sec
     serial.writeString("!1:LIGHT:" + input.lightLevel() + "#")
-    basic.pause(5000)
+}
+let cmd = ""
+let countLIGHT = 0
+let countTEMP = 0
+let sec = 0
+basic.showIcon(IconNames.Heart)
+sec = 0
+countTEMP = 0
+countLIGHT = 5
+basic.forever(function () {
+    basic.pause(1000)
+    sec += 1
+    if (sec - countTEMP >= 10) {
+        sendTEMP()
+    }
+    if (sec - countLIGHT >= 10) {
+        sendLIGHT()
+    }
 })
